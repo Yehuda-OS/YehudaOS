a803b9882806271d8e5f8c9c7309c49bbda4c4c2
Noam Broza <noambroza@gmail.com>
2022-11-13 12:29:24 +0200

    added function to load page tables to cr3

--- kernel/src/paging/mod.rs
+++ kernel/src/paging/mod.rs
@@ -1,4 +1,11 @@
 pub mod virtual_memory_manager;
 pub mod page_allocator;
 
+use x86_64::PhysAddr;
+
 const HHDM_OFFSET: u64 = 0xffff_8000_0000_0000;
+
+pub unsafe fn load_tables_to_cr3(p4_addr: PhysAddr) {
+    use x86_64::{registers::control::{Cr3, Cr3Flags}, structures::paging::{Size4KiB, PhysFrame}};
+    Cr3::write(PhysFrame::<Size4KiB>::from_start_address(p4_addr).expect("error"), Cr3Flags::empty()) 
+}
