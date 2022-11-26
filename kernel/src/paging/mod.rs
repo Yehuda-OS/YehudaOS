1581244cc486728f66c3d484c05b397c1e375a96
Omer <omeralon53@gmail.com>
2022-11-26 20:40:48 +0200

    Create a function that maps the kernel's address

--- kernel/src/paging/mod.rs
+++ kernel/src/paging/mod.rs
@@ -1,11 +1,19 @@
 pub mod page_allocator;
 pub mod virtual_memory_manager;
 
-use limine::{LimineHhdmRequest, LimineMemmapEntry, LimineMemmapRequest, LimineMemmapResponse};
-use x86_64::PhysAddr;
+use limine::{
+    LimineHhdmRequest, LimineKernelAddressRequest, LimineMemmapEntry, LimineMemmapRequest,
+    LimineMemmapResponse, LimineMemoryMapEntryType,
+};
+use x86_64::{
+    registers::control::{Cr3, Cr3Flags},
+    structures::paging::{PageSize, PageTableFlags, PhysFrame, Size4KiB},
+    PhysAddr, VirtAddr,
+};
 
 const PAGE_TABLE_ENTRIES: isize = 512;
 
+static KERNEL_ADDRESS: LimineKernelAddressRequest = LimineKernelAddressRequest::new(0);
 static HHDM: LimineHhdmRequest = LimineHhdmRequest::new(0);
 static mut HHDM_OFFSET: u64 = 0;
 static MEMMAP: LimineMemmapRequest = LimineMemmapRequest::new(0);
@@ -35,13 +43,43 @@ unsafe fn get_memmap_entry(memmap: &LimineMemmapResponse, i: u64) -> &LimineMemm
 /// # Safety
 /// The function is unsafe because changing the page table can lead to a memory violation.
 pub unsafe fn load_tables_to_cr3(p4_addr: PhysAddr) {
-    use x86_64::{
-        registers::control::{Cr3, Cr3Flags},
-        structures::paging::{PhysFrame, Size4KiB},
-    };
     Cr3::write(
         // UNWRAP: the page frame allocator will make sure that the address will be 4KiB aligned.
         PhysFrame::<Size4KiB>::from_start_address(p4_addr).unwrap(),
         Cr3Flags::empty(),
     );
+}
+
+/// Map the kernel's virtual address.
+///
+/// # Arguments
+/// * `pml4` - The page map level 4, the highest page table.
+pub fn map_kernel_address(pml4: PhysAddr) {
+    let memmap = get_memmap();
+    let virtual_address = KERNEL_ADDRESS.get_response().get().unwrap().virtual_base;
+    let flags = PageTableFlags::GLOBAL | PageTableFlags::PRESENT;
+    let mut entry;
+    let mut offset = 0;
+
+    for i in 0..memmap.entry_count {
+        // UNSAFE: `i` is between 0 and the entry count.
+        entry = unsafe { get_memmap_entry(memmap, i) };
+
+        if entry.typ == LimineMemoryMapEntryType::KernelAndModules {
+            while offset < entry.len {
+                let physical =
+                    PhysFrame::<Size4KiB>::from_start_address(PhysAddr::new(entry.base + offset))
+                        .unwrap();
+
+                virtual_memory_manager::map_address(
+                    pml4,
+                    VirtAddr::new(virtual_address + offset),
+                    physical,
+                    flags,
+                );
+                offset += Size4KiB::SIZE;
+            }
+            break;
+        }
+    }
 }
