26b05c2ca2a331144da36de18241871421c60132
Omer <omeralon53@gmail.com>
2022-12-12 13:05:25 +0200

    Improve the gdb script

--- kernel/run-gdb.sh
+++ kernel/run-gdb.sh
@@ -1,2 +1,4 @@
-gnome-terminal -- rust-gdb -ex "target remote :1234" -ex "file target/x86_64-unknown-none/debug/kernel"
+gnome-terminal -- rust-gdb -ex "target remote :1234" \
+-ex "file target/x86_64-unknown-none/debug/kernel" \
+-ex "b _start" -ex "c" -ex "layout src"
 cargo run -- "-s -S"
