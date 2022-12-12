2ce41688546fc7f9970d015506d29ba714e69a13
Omer <omeralon53@gmail.com>
2022-12-12 13:05:34 +0200

    Hard code addresses that needs to be mapped

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,7 +3,14 @@
 #![feature(alloc_error_handler)]
 
 extern crate alloc;
-use x86_64::registers::control::Cr3;
+use limine::{LimineFramebufferRequest, LimineTerminalRequest};
+use x86_64::{
+    registers::control::Cr3,
+    structures::paging::{PageTableFlags, PhysFrame, Size1GiB, Size2MiB, Size4KiB},
+    PhysAddr, VirtAddr,
+};
+
+use crate::memory::virtual_memory_manager::virtual_to_physical;
 
 mod io;
 mod memory;
@@ -29,8 +36,21 @@ pub extern "C" fn _start() -> ! {
         memory::PAGE_TABLE = memory::virtual_memory_manager::create_page_table();
         memory::map_kernel_address(memory::PAGE_TABLE);
         memory::create_hhdm(memory::PAGE_TABLE);
+        memory::virtual_memory_manager::map_address(
+            memory::PAGE_TABLE,
+            VirtAddr::new(0x000000000002910b),
+            PhysFrame::<Size1GiB>::containing_address(PhysAddr::new(0x0)),
+            PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::HUGE_PAGE,
+        );
+        memory::virtual_memory_manager::map_address(
+            memory::PAGE_TABLE,
+            VirtAddr::new(0x00000000fd000190),
+            PhysFrame::<Size2MiB>::containing_address(PhysAddr::new(0x00000000fd000000)),
+            PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::HUGE_PAGE,
+        );
         memory::load_tables_to_cr3(memory::PAGE_TABLE);
     }
+    println!("Hello world");
 
     hcf();
 }
