9212a416061f93085fe8f4995f93d226d4928978
NoamBroza <noambroza@gmail.com>
2022-12-13 01:02:23 +0200

    updated BlkDev and cargo.toml of the fs
    - new format for the BlkDev
    - fixed the fs-rs cargo.toml
    - added fs-rs to the kernel includes in the cargo.toml

--- fs-rs/src/fs/blkdev/mod.rs
+++ fs-rs/src/fs/blkdev/mod.rs
@@ -1,5 +1,6 @@
 extern crate alloc;
 use alloc::vec::Vec;
+use core::result::Result::*;
 
 pub struct BlkDev(Vec<u8>); // BlkDev.0 is the file map
 
@@ -10,8 +11,18 @@ impl core::clone::Clone for BlkDev {
 }
 
 impl BlkDev {
-    pub fn new(filemap: Vec<u8>) -> Self {
-        Self(filemap)
+    pub const DEVICE_SIZE: usize = 1024 * 1024;
+
+    fn new(data: Vec<u8>) -> Result<Self, core::fmt::Error> {
+        // Set the initial data of the block device to the provided data
+        let mut filemap = data;
+
+        // Ensure that the block device has at least the minimum required size
+        if filemap.len() < Self::DEVICE_SIZE {
+            filemap.resize(Self::DEVICE_SIZE, 0);
+        }
+
+        Ok(BlkDev(filemap))
     }
 
     pub unsafe fn read(self, addr: isize, size: usize, ans: *mut u8) {
@@ -23,6 +34,4 @@ impl BlkDev {
         let dst: *mut u8 = (self.0.as_ptr().addr() as isize + addr) as usize as *mut u8;
         core::ptr::copy_nonoverlapping(data, dst, size);
     }
-
-    pub const DEVICE_SIZE: usize = 1024 * 1024;
 }
