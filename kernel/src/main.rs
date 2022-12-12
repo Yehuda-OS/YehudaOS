1d8f2886d57a074211da4e82d6677f21b4b7de4a
Omer <omeralon53@gmail.com>
2022-12-12 21:06:53 +0200

    Correctly initialize our page tables

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,14 +3,6 @@
 #![feature(alloc_error_handler)]
 
 extern crate alloc;
-use limine::{LimineFramebufferRequest, LimineTerminalRequest};
-use x86_64::{
-    registers::control::Cr3,
-    structures::paging::{PageTableFlags, PhysFrame, Size1GiB, Size2MiB, Size4KiB},
-    PhysAddr, VirtAddr,
-};
-
-use crate::memory::virtual_memory_manager::virtual_to_physical;
 
 mod io;
 mod memory;
@@ -22,33 +14,13 @@ mod memory;
 /// the bootloader will transfer control to this function.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let theirs = Cr3::read().0.start_address();
-
-    unsafe {
-        memory::allocator::ALLOCATOR =
-            memory::allocator::Locked::<memory::allocator::Allocator>::new(
-                memory::allocator::Allocator::new(memory::allocator::HEAP_START, theirs),
-            )
-    };
-
     memory::page_allocator::initialize();
     unsafe {
         memory::PAGE_TABLE = memory::virtual_memory_manager::create_page_table();
         memory::map_kernel_address(memory::PAGE_TABLE);
         memory::create_hhdm(memory::PAGE_TABLE);
-        memory::virtual_memory_manager::map_address(
-            memory::PAGE_TABLE,
-            VirtAddr::new(0x000000000002910b),
-            PhysFrame::<Size1GiB>::containing_address(PhysAddr::new(0x0)),
-            PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::HUGE_PAGE,
-        );
-        memory::virtual_memory_manager::map_address(
-            memory::PAGE_TABLE,
-            VirtAddr::new(0x00000000fd000190),
-            PhysFrame::<Size2MiB>::containing_address(PhysAddr::new(0x00000000fd000000)),
-            PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::HUGE_PAGE,
-        );
         memory::load_tables_to_cr3(memory::PAGE_TABLE);
+        memory::map_framebuffer();
     }
     println!("Hello world");
 
