ff36ec2d41cf14d81217d5778b8ad977542e2bf5
Omer <omeralon53@gmail.com>
2023-01-06 13:19:21 +0200

    Move the functions to the bottom

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -14,6 +14,68 @@ static mut GDT: [Entry; 6] = [
     Entry::zeros(),
 ];
 
+
+#[repr(packed)]
+#[allow(unused)]
+struct Entry {
+    limit0: u16,
+    base0: u16,
+    base1: u8,
+    access: AccessByte,
+    limit1_flags: u8,
+    base2: u8,
+}
+
+bitflags! {
+    struct AccessByte: u8 {
+        /// If set on a data segment, the segment will be writable and if it is set on a code
+        /// segment the segment will be readable.
+        const READABLE_WRITEABLE = 1 << 1;
+        const EXECUTABLE = 1 << 3;
+        /// If not set, the segment will be a system segment.
+        const CODE_OR_DATA = 1 << 4;
+        /// If set, the segment will be accessible from ring 3.
+        const RING3 = 1 << 4 | 1 << 5;
+        const PRESENT = 1 << 7;
+
+        /// `Available 32-bit TSS` type of a system segment.
+        const TYPE_TSS = 0x9;
+    }
+
+    struct Flags: u8 {
+        /// Must be set in 64 bit code segments.
+        const LONG_MODE = 1 << 1;
+        /// This is set in data segment.
+        const DEFAULT_SIZE = 1 << 2;
+        /// If set, the limit is a count of 4KiB blocks instead of 1 byte blocks.
+        const GRANULARITY_4KIB = 1 << 3;
+    }
+}
+
+impl Entry {
+    pub const fn new(base: u64, limit: u32, access: AccessByte, flags: Flags) -> Self {
+        Entry {
+            limit0: limit as u16,
+            base0: base as u16,
+            base1: (base >> 16) as u8,
+            access,
+            limit1_flags: (flags.bits << 4) | (limit >> 16) as u8,
+            base2: (base >> 24) as u8,
+        }
+    }
+
+    pub const fn zeros() -> Self {
+        Entry {
+            limit0: 0,
+            base0: 0,
+            base1: 0,
+            access: AccessByte { bits: 0 },
+            limit1_flags: 0,
+            base2: 0,
+        }
+    }
+}
+
 /// Create the GDT with the required segments.
 pub fn create() {
     unsafe {
@@ -124,65 +186,4 @@ pub unsafe fn activate() {
     core::arch::asm!("lgdt [{gdt_descriptor}]", gdt_descriptor=in(reg)gdt_descriptor);
     reload_segments();
     loop {}
-}
-
-#[repr(packed)]
-#[allow(unused)]
-struct Entry {
-    limit0: u16,
-    base0: u16,
-    base1: u8,
-    access: AccessByte,
-    limit1_flags: u8,
-    base2: u8,
-}
-
-bitflags! {
-    struct AccessByte: u8 {
-        /// If set on a data segment, the segment will be writable and if it is set on a code
-        /// segment the segment will be readable.
-        const READABLE_WRITEABLE = 1 << 1;
-        const EXECUTABLE = 1 << 3;
-        /// If not set, the segment will be a system segment.
-        const CODE_OR_DATA = 1 << 4;
-        /// If set, the segment will be accessible from ring 3.
-        const RING3 = 1 << 4 | 1 << 5;
-        const PRESENT = 1 << 7;
-
-        /// `Available 32-bit TSS` type of a system segment.
-        const TYPE_TSS = 0x9;
-    }
-
-    struct Flags: u8 {
-        /// Must be set in 64 bit code segments.
-        const LONG_MODE = 1 << 1;
-        /// This is set in data segment.
-        const DEFAULT_SIZE = 1 << 2;
-        /// If set, the limit is a count of 4KiB blocks instead of 1 byte blocks.
-        const GRANULARITY_4KIB = 1 << 3;
-    }
-}
-
-impl Entry {
-    pub const fn new(base: u64, limit: u32, access: AccessByte, flags: Flags) -> Self {
-        Entry {
-            limit0: limit as u16,
-            base0: base as u16,
-            base1: (base >> 16) as u8,
-            access,
-            limit1_flags: (flags.bits << 4) | (limit >> 16) as u8,
-            base2: (base >> 24) as u8,
-        }
-    }
-
-    pub const fn zeros() -> Self {
-        Entry {
-            limit0: 0,
-            base0: 0,
-            base1: 0,
-            access: AccessByte { bits: 0 },
-            limit1_flags: 0,
-            base2: 0,
-        }
-    }
 }
