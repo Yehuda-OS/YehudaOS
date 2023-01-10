34f491ed7c6ab35d5a1e42f749d40244cc541472
Omer <omeralon53@gmail.com>
2023-01-10 15:16:20 +0200

    Load the context of a process
    Create a struct to store a process' registers
    
    Create a struct to store a process' data
    
    Load the rsp to the TSS when loading the TSS to the task register
    
    Create a function that loads the context of a process

--- kernel/src/scheduler.rs
+++ kernel/src/scheduler.rs
@@ -1,3 +1,9 @@
+use core::arch::asm;
+use x86_64::PhysAddr;
+
+const CODE_SEGMENT: u16 = super::gdt::USER_CODE | 3;
+const DATA_SEGMENT: u16 = super::gdt::USER_DATA | 3;
+
 static mut TSS_ENTRY: TaskStateSegment = TaskStateSegment {
     reserved0: 0,
     rsp0: 0,
@@ -36,15 +42,88 @@ pub struct TaskStateSegment {
     io_permission_bitmap: u16,
 }
 
+#[repr(packed)]
+pub struct Registers {
+    pub rax: u64,
+    pub rbx: u64,
+    pub rcx: u64,
+    pub rdx: u64,
+    pub rsi: u64,
+    pub rdi: u64,
+    pub rbp: u64,
+    pub r8: u64,
+    pub r9: u64,
+    pub r10: u64,
+    pub r11: u64,
+    pub r12: u64,
+    pub r13: u64,
+    pub r14: u64,
+    pub r15: u64,
+}
+
+pub struct Process {
+    pub registers: Registers,
+    pub page_table: PhysAddr,
+    pub stack_pointer: u64,
+    pub instruction_pointer: u64,
+    pub flags: u64,
+}
+
 /// Returns the address of the Task State Segment.
 pub fn get_tss_address() -> u64 {
     unsafe { &TSS_ENTRY as *const _ as u64 }
 }
 
-/// Load the tss segment selector to the task register.
-/// 
+/// Load kernel's stack pointer to the TSS and load the
+/// TSS segment selector to the task register.
+///
 /// # Safety
 /// This function is unsafe because it requires a valid GDT with a TSS segment descriptor.
 pub unsafe fn load_tss() {
-    core::arch::asm!("ltr ax", in("ax")super::gdt::TSS);
+    asm!("mov {0}, rsp", out(reg)TSS_ENTRY.rsp0);
+    asm!("ltr ax", in("ax")super::gdt::TSS);
+}
+
+pub unsafe fn load_context(p: &Process) {
+    asm!("
+    mov ds, {0:x}
+    mov es, {0:x}
+    mov fs, {0:x}
+    mov gs, {0:x}
+
+    push {0:r}
+    push {rsp}
+    pushfq
+    push {1:r}
+    push {rip}
+    ",
+        in(reg)DATA_SEGMENT, in(reg)CODE_SEGMENT,
+        rsp=in(reg)p.stack_pointer, rip=in(reg)p.instruction_pointer
+    );
+    asm!("
+    push {rbx}
+    push {rbp}
+    ",
+            rbx=in(reg)p.registers.rbx,
+            rbp=in(reg)p.registers.rbp,
+    );
+
+    asm!("
+    pop rbp
+    pop rbx
+    iretq",
+        in("rax")p.registers.rax,
+        in("rcx")p.registers.rcx,
+        in("rdx")p.registers.rdx,
+        in("rsi")p.registers.rsi,
+        in("rdi")p.registers.rdi,
+        in("r8")p.registers.r8,
+        in("r9")p.registers.r9,
+        in("r10")p.registers.r10,
+        in("r11")p.registers.r11,
+        in("r12")p.registers.r12,
+        in("r13")p.registers.r13,
+        in("r14")p.registers.r14,
+        in("r15")p.registers.r15,
+    );
 }
