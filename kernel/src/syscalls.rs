d49de6e244314031a63ce1b3b8fd083ae876b741
Omer <omeralon53@gmail.com>
2023-01-25 18:45:57 +0200

    Save the context of the process
    Save the context in the syscalls handler

--- kernel/src/syscalls.rs
+++ kernel/src/syscalls.rs
@@ -1,4 +1,5 @@
 use super::io;
+use super::scheduler;
 
 const EFER: u32 = 0xc0000080;
 const STAR: u32 = 0xc0000081;
@@ -14,8 +15,29 @@ pub unsafe fn initialize() {
     io::wrmsr(EFER, 1);
 }
 
-pub unsafe fn handler() {
+pub unsafe fn handler() -> ! {
+    let registers = scheduler::save_context();
+    // TODO Change later to get the currently running process.
+    let mut proc = scheduler::Process {
+        registers,
+        // After we change this to the running process the page table field will already be loaded.
+        page_table: unsafe { super::memory::PAGE_TABLE },
+        stack_pointer: 0,
+        instruction_pointer: 0,
+        flags: 0,
+    };
+
+    // The `syscall` instruction saves the instruction pointer in `rcx` and the cpu flags in `r11`.
+    proc.instruction_pointer = proc.registers.rcx;
+    proc.flags = proc.registers.r11;
+    // `rbp` holds the value of the stack pointer after pushing the original `rbp`.
+    core::arch::asm!("
+    mov {0}, rbp
+    sub {0}, 8
+    ",
+        out(reg)proc.stack_pointer,
+    );
     crate::println!("A syscall occured");
 
-    loop {}
+    scheduler::load_context(&proc);
 }
