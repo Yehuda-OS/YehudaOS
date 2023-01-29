d4fa1f7a031648c0d4ca7fc442398ebc6e1b84e2
NoamBroza <noambroza@gmail.com>
2023-01-29 14:54:33 +0200

    added process states enum

--- kernel/src/scheduler.rs
+++ kernel/src/scheduler.rs
@@ -65,6 +65,15 @@ pub struct Registers {
     pub r15: u64,
 }
 
+#[repr(u8)]
+pub enum ProcessStates {
+    New,
+    Ready,
+    Running,
+    Waiting,
+    Terminate,
+}
+
 pub struct Process {
     pub registers: Registers,
     pub page_table: PhysAddr,
