facc76d00d83df60ea8b6bdace89f6191e92dc3f
Omer <omeralon53@gmail.com>
2023-01-30 13:43:35 +0200

    Get the program header table

--- kernel/src/scheduler/loader.rs
+++ kernel/src/scheduler/loader.rs
@@ -25,20 +25,42 @@ struct ElfEhdr {
     e_type: u16,
     e_machine: u16,
     e_version: u32,
+    /// Entry point of the process
     e_entry: ElfAddr,
+    /// Program header offset
     e_phoff: ElfOff,
     e_shoff: ElfOff,
     e_flags: u32,
     e_ehsize: u16,
+    /// Program header entry size
     e_phentsize: u16,
+    /// Program header entry count
     e_phnum: u16,
     e_shentsize: u16,
     e_shnum: u16,
     e_shstrndx: u16,
 }
 
+#[repr(C)]
+#[derive(Default, Clone)]
+struct ElfPhdr {
+    p_type: u32,
+    p_flags: u32,
+    p_offset: ElfOff,
+    p_vaddr: ElfAddr,
+    p_paddr: ElfAddr,
+    p_filesz: u64,
+    p_memsz: u64,
+    p_align: u64,
+}
+
+/// Returns the header of the ELF file.
+///
+/// # Arguments
+/// - `file_id` - The ID of the ELF file.
 fn get_header(file_id: u64) -> ElfEhdr {
     let mut header = ElfEhdr::default();
+    // SAFETY: The header is of the size of `ElfEhdr`.
     let header_slice = unsafe {
         core::slice::from_raw_parts_mut(
             &mut header as *mut _ as *mut u8,
@@ -53,15 +75,37 @@ fn get_header(file_id: u64) -> ElfEhdr {
     header
 }
 
-pub fn load_process(file_id: u64) -> Process {
+/// Returns an array of the program header pointer.
+///
+/// # Arguments
+/// - `file_id` - The ID of the ELF file.
+/// - `header` - The header of the ELF file.
+fn get_program_table(file_id: u64, header: &ElfEhdr) -> alloc::vec::Vec<ElfPhdr> {
+    let mut buffer = alloc::vec![ElfPhdr::default(); header.e_phnum as usize];
+
+    unsafe {
+        fs::read(
+            file_id as usize,
+            core::slice::from_raw_parts_mut(
+                buffer.as_mut_ptr() as *mut u8,
+                buffer.len() * header.e_phentsize as usize,
+            ),
+            header.e_phoff as usize,
+        );
+
+        buffer
+    }
+}
+
+pub unsafe fn load_process(file_id: u64) -> Option<Process> {
     let header = get_header(file_id);
     let p = Process {
         registers: super::Registers::default(),
-        page_table: unsafe { crate::memory::PAGE_TABLE },
+        page_table: super::create_page_table()?,
         stack_pointer: 0,
         instruction_pointer: header.e_entry,
         flags: 0,
     };
 
-    p
+    Some(p)
 }
