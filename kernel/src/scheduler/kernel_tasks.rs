1f96255f7731c5fcc183dfb726273446829752e7
Omer <omeralon53@gmail.com>
2023-02-08 13:01:57 +0200

    Implement `deallocate_stack` in `kernel_tasks`

--- kernel/src/scheduler/kernel_tasks.rs
+++ kernel/src/scheduler/kernel_tasks.rs
@@ -1,5 +1,9 @@
-use x86_64::structures::paging::{PageSize, Size4KiB};
+use x86_64::{
+    structures::paging::{PageSize, PhysFrame, Size4KiB},
+    VirtAddr,
+};
 
+use crate::memory;
 use crate::mutex::Mutex;
 
 use super::SchedulerError;
@@ -8,6 +12,11 @@ const MAX_STACK_SIZE: u64 = 1024 * 4 * 20; // 80KiB
 const STACK_START: u64 = 0x2000_0000;
 
 static STACK_BITMAP: Mutex<u64> = Mutex::new(0);
+
+fn get_stack_address(index: u64) -> u64 {
+    // There is one page of unused space between the stacks.
+    STACK_START + index * (MAX_STACK_SIZE + Size4KiB::SIZE)
+}
 
 /// Allocates a virtual address for a kernel's task stack.
 ///
@@ -23,8 +32,7 @@ fn allocate_stack() -> Option<u64> {
             // Set the stack as used.
             *bitmap ^= 1 << i;
 
-            // There is one page of unused space between the stacks.
-            return Some(STACK_START + i * (MAX_STACK_SIZE + Size4KiB::SIZE));
+            return Some(get_stack_address(i));
         }
     }
 
@@ -33,12 +41,29 @@ fn allocate_stack() -> Option<u64> {
 }
 
 /// Free a kernel's task stack after it is finished.
+/// Frees all the memory that was mapped to this stack and marks it as free in the bitmap.
 /// 
 /// # Arguments
 /// - `stack_pointer` - The stack pointer of the kernel's stack.
 /// The function assumes the stack pointer is in the range of the stack the task has received.
-fn deallocate_stack(stack_pointer: u64) {
-    todo!()
+pub fn deallocate_stack(stack_pointer: u64) {
+    let index = (stack_pointer + MAX_STACK_SIZE - STACK_START) / (MAX_STACK_SIZE + Size4KiB::SIZE);
+    // Get the lower edge of the stack.
+    let lower = VirtAddr::new(get_stack_address(index - 1) + Size4KiB::SIZE);
+    let higher = VirtAddr::new(get_stack_address(index));
+
+    for addr in (lower..higher).step_by(Size4KiB::SIZE as usize) {
+        if let Ok(page) = memory::vmm::virtual_to_physical(memory::get_page_table(), addr) {
+            // UNWRAP: The entry is unused because we checked if it is mapped
+            // and the page table should not be null.
+            memory::vmm::unmap_address(page, addr).unwrap();
+            // UNWRAP: The page was returned from the `virtual_to_physical` function.
+            unsafe { memory::page_allocator::free(PhysFrame::from_start_address(page).unwrap()) }
+        }
+    }
+
+    // Clear the stack's bit.
+    *STACK_BITMAP.lock() &= !(1 << index);
 }
 
 impl super::Process {
