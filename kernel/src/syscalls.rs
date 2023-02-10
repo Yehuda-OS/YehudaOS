d4fba2d9c4183c06bd0f64553a0ca16c7bb10fce
Omer <omeralon53@gmail.com>
2023-02-10 09:48:15 +0200

    Rework on the syscalls handler
    Use the currently running process
    
    Handle the exit syscall inside the handler

--- kernel/src/syscalls.rs
+++ kernel/src/syscalls.rs
@@ -13,6 +13,10 @@ const STDOUT_DESCRIPTOR: i32 = 1;
 const STDERR_DESCRIPTOR: i32 = 2;
 const TO_SUB_FROM_FD_TO_GET_FILE_ID: i32 = 3;
 
+mod syscall {
+    pub const EXIT: u64 = 0x3c;
+}
+
 pub unsafe fn initialize() {
     let rip = handler as u64;
     let cs = u64::from(super::gdt::KERNEL_CODE) << 32;
@@ -33,71 +37,68 @@ pub unsafe fn initialize() {
 /// - `arg3` - Stored in `r10`.
 /// - `arg4` - Stored in `r8`.
 /// - `arg5` - Stored in `r9`.
-pub fn handle_syscall(
-    syscall_number: u64,
-    arg0: u64,
-    arg1: u64,
-    arg2: u64,
-    arg3: u64,
-    arg4: u64,
-    arg5: u64,
-) {
+pub fn handle_syscall(registers: &scheduler::Registers) -> i64 {
+    let syscall_number = registers.rax;
+    let arg0 = registers.rdi;
+    let arg1 = registers.rsi;
+    let arg2 = registers.rdx;
+    let arg3 = registers.r10;
+    let arg4 = registers.r8;
+    let arg5 = registers.r9;
+
+    match syscall_number {
+        _ => -1,
+    }
 }
 
 pub unsafe fn int_0x80_handler() {
     let registers = super::scheduler::save_context();
 
-    handle_syscall(
-        registers.rax,
-        registers.rdi,
-        registers.rsi,
-        registers.rdx,
-        registers.r10,
-        registers.r8,
-        registers.r9,
-    );
+    handle_syscall(&registers);
 
     loop {}
 }
 
-pub unsafe fn handler() -> ! {
-    let registers = scheduler::save_context();
-    // TODO Change later to get the currently running process.
-    let mut proc = scheduler::Process {
-        registers,
-        // After we change this to the running process the page table field will already be loaded.
-        page_table: super::memory::get_page_table(),
-        stack_pointer: 0,
-        instruction_pointer: 0,
-        flags: 0,
-        kernel_task: false,
-    };
-
-    // Disable interrupts while handling a syscall.
-    core::arch::asm!("cli");
-
+/// Update the registers of a proccess when a syscall occurs.
+/// 
+/// # Arguments
+/// - `proc` - A mutable reference to the process.
+/// - `registers` - The process' registers.
+fn update_registers(proc: &mut scheduler::Process, registers: &scheduler::Registers) {
+    proc.registers = *registers;
     // The `syscall` instruction saves the instruction pointer in `rcx` and the cpu flags in `r11`.
     proc.instruction_pointer = proc.registers.rcx;
     proc.flags = proc.registers.r11;
-    // `rbp` holds the value of the stack pointer after pushing the original `rbp`.
-    core::arch::asm!("
-    mov {0}, rbp
-    add {0}, 8
-    ",
-        out(reg)proc.stack_pointer,
-    );
-    crate::println!("A syscall occured");
-    handle_syscall(
-        proc.registers.rax,
-        proc.registers.rdi,
-        proc.registers.rsi,
-        proc.registers.rdx,
-        proc.registers.r10,
-        proc.registers.r8,
-        proc.registers.r9,
-    );
+    // SAFETY: `rbp` holds the value of the stack pointer after pushing the original `rbp`.
+    unsafe {
+        core::arch::asm!("
+        mov {0}, rbp
+        add {0}, 8
+        ",
+            out(reg)proc.stack_pointer,
+        );
+    }
+}
 
-    loop {}
+pub unsafe fn handler() -> ! {
+    let mut registers = scheduler::save_context();
+    let mut currently_running = scheduler::get_running_process();
+
+    // Disable interrupts while handling a syscall.
+    core::arch::asm!("cli");
+    crate::println!("A syscall occured");
+
+    // If the syscall is `exit` it must be handled here because we have the reference to the
+    // currently running process here.
+    if registers.rax == syscall::EXIT {
+        *currently_running = None;
+    } else {
+        registers.rax = handle_syscall(&registers) as u64;
+        // UNWRAP: Syscalls should not be called from inside the kernel.
+        update_registers(currently_running.as_mut().unwrap(), &registers);
+    }
+
+    scheduler::load_from_queue();
 }
 
 /// implementation for `read` syscall
