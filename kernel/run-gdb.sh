18352d15d04fb203434a8cce6d13b50d7012a24c
Omer <omeralon53@gmail.com>
2023-02-12 18:39:20 +0200

    Change the gdb script

--- kernel/run-gdb.sh
+++ kernel/run-gdb.sh
@@ -1,5 +1,4 @@
-cargo run -- "-s -S" &
+gnome-terminal -- cargo run -- "-s -S"
 rust-gdb -ex "target remote :1234" \
 -ex "file target/x86_64-unknown-none/debug/kernel" \
 -ex "b _start" -ex "c" -ex "layout src"
-
