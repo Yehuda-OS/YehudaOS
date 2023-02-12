f7bec756a49697a38c8c71c09c7752cde0eb7af1
Omer <omeralon53@gmail.com>
2023-02-12 18:04:09 +0200

    Use the current terminal in the gdb script

--- kernel/run-gdb.sh
+++ kernel/run-gdb.sh
@@ -1,4 +1,5 @@
-gnome-terminal -- rust-gdb -ex "target remote :1234" \
+cargo run -- "-s -S" &
+rust-gdb -ex "target remote :1234" \
 -ex "file target/x86_64-unknown-none/debug/kernel" \
 -ex "b _start" -ex "c" -ex "layout src"
-cargo run -- "-s -S"
+
