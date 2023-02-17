3ad3bbb03d1d455ef736e216f37716d8e3028eb6
Omer <omeralon53@gmail.com>
2023-02-17 11:29:23 +0200

    Save the context in the PIT
    Create a function that saves the general purpose registers and calls
    the handler with the stack frame as a parameter

--- kernel/src/pit.rs
+++ kernel/src/pit.rs
@@ -1,3 +1,9 @@
+use core::arch::asm;
+
+use x86_64::structures::idt::InterruptStackFrame;
+
+use crate::{memory, scheduler};
+
 use super::io;
 
 const TICKS_PER_SECOND: u32 = 1193182;
@@ -20,11 +26,52 @@ pub unsafe fn start(tps: u32) {
     io::outb(PIT_COMMAND_PORT, PIT_COMMAND);
     io::outb(PIT_CHANNEL0, low);
     io::outb(PIT_CHANNEL0, high);
-    core::arch::asm!("sti");
 }
 
-pub extern "x86-interrupt" fn handler(_stack_frame: x86_64::structures::idt::InterruptStackFrame) {
-    unsafe {
-        super::idt::PICS.lock().notify_end_of_interrupt(0x20);
-    }
+/// Save the general purpose registers of the process and run the handler.
+///
+/// # Safety
+/// This operation assumes it was triggered from the pit interrupt
+/// during the execution of a process in ring 3.
+#[naked]
+pub unsafe extern "C" fn handler_save_context() {
+    asm!(
+        "
+        mov gs:0, rax
+        mov gs:8, rbx
+        mov gs:16, rcx
+        mov gs:24, rdx
+        mov gs:32, rsi
+        mov gs:40, rdi
+        mov gs:48, rbp
+        mov gs:56, r8
+        mov gs:64, r9
+        mov gs:72, r10
+        mov gs:80, r11
+        mov gs:88, r12
+        mov gs:96, r13
+        mov gs:104, r14
+        mov gs:112, r15
+        swapgs
+        mov rdi, rsp
+        call pit_handler
+    ",
+        options(noreturn)
+    );
+}
+
+#[no_mangle]
+unsafe extern "C" fn pit_handler(frame: &InterruptStackFrame) {
+    let curr = scheduler::get_running_process().as_mut().unwrap();
+
+    memory::load_tables_to_cr3(memory::get_page_table());
+    curr.instruction_pointer = (*frame).instruction_pointer.as_u64();
+    curr.stack_pointer = (*frame).stack_pointer.as_u64();
+    curr.flags = (*frame).cpu_flags;
+
+    crate::print!(".");
+
+    scheduler::switch_current_process();
+    super::idt::PICS.lock().notify_end_of_interrupt(0x20);
+    scheduler::load_from_queue();
 }
