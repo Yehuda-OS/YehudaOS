a99528fce7ab228b7c539b76b3c722e4099fc0c6
Omer <omeralon53@gmail.com>
2023-04-09 17:07:37 +0300

    Implement syscall wrappers

--- usermode/yehuda-os.c
+++ usermode/yehuda-os.c
@@ -30,3 +30,77 @@ syscall(size_t syscall_number, size_t arg0, size_t arg1, size_t arg2, size_t arg
     return result;
 }
 
+int read(int fd, void* buf, size_t count, size_t offset)
+{
+    return syscall(READ, fd, (size_t)buf, count, offset, 0, 0);
+}
+
+int write(int fd, const void* buf, size_t count, size_t offset)
+{
+    return syscall(WRITE, fd, (size_t)buf, count, offset, 0, 0);
+}
+
+int open(const char* pathname)
+{
+    return syscall(OPEN, (size_t)pathname, 0, 0, 0, 0, 0);
+}
+
+int fstat(int fd, struct Stat* statbuf)
+{
+    return syscall(FSTAT, fd, (size_t)statbuf, 0, 0, 0, 0);
+}
+
+void* malloc(size_t size)
+{
+    return (void*)syscall(MALLOC, size, 0, 0, 0, 0, 0);
+}
+
+void free(void* ptr)
+{
+    syscall(FREE, (size_t)ptr, 0, 0, 0, 0, 0);
+}
+
+int exec(const char* pathname)
+{
+    return syscall(EXEC, (size_t)pathname, 0, 0, 0, 0, 0);
+}
+
+void exit(int status)
+{
+    syscall(EXIT, (size_t)status, 0, 0, 0, 0, 0);
+    // `syscall` will never return when the `EXIT` code is passed.
+    // Therefore we put this while loop to suppress any warnings.
+    while (1)
+    {
+    }
+}
+
+int fchdir(int fd)
+{
+    return syscall(FCHDIR, fd, 0, 0, 0, 0, 0);
+}
+
+int creat(const char* path, bool_t directory)
+{
+    return syscall(CREAT, (size_t)path, (size_t)directory, 0, 0, 0, 0);
+}
+
+int remove_file(const char* path)
+{
+    return syscall(REMOVE_FILE, (size_t)path, 0, 0, 0, 0, 0);
+}
+
+int readdir(int fd, size_t offset, struct DirEntry* dirp)
+{
+    return syscall(READ_DIR, fd, offset, (size_t)dirp, 0, 0, 0);
+}
+
+int truncate(const char* path, size_t length)
+{
+    return syscall(TRUNCATE, (size_t)path, length, 0, 0, 0, 0);
+}
+
+int ftruncate(int fd, size_t length)
+{
+    return syscall(FTRUNCATE, fd, length, 0, 0, 0, 0);
+}
