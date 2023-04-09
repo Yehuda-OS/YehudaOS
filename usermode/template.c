fa98721e062d2d4100e8ee34854c0ac4db5be371
Omer <omeralon53@gmail.com>
2023-04-09 11:56:07 +0300

    Create a directory for the usermode applications

---
+++ usermode/template.c
@@ -0,0 +1,18 @@
+// Tell the compiler incoming stack alignment is not RSP%16==8 or ESP%16==12
+__attribute__((force_align_arg_pointer)) void _start()
+{
+    asm("call main");
+
+    /* exit system call */
+    asm("mov $0, %rdi;"
+        "mov %eax, %edi;"
+        "mov $0x3c, %rax;"
+        "syscall");
+    // tell the compiler to make sure side effects are done before the asm statement
+    __builtin_unreachable();
+}
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
