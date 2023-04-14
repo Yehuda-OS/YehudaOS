325ba2706fdce2a1dd1bbab089aee0c20136b739
Omer <omeralon53@gmail.com>
2023-04-14 09:22:38 +0300

    Create the file shell.c

---
+++ usermode/shell.c
@@ -0,0 +1,35 @@
+#include "yehuda-os.h"
+
+const char* executable_path_start[] = { "./", "../", "/" };
+
+void handle_builtin()
+{
+}
+
+void handle_executable()
+{
+}
+
+char** parse_command(char* command)
+{
+    return NULL;
+}
+
+int main()
+{
+    return 0;
+}
+
+// Tell the compiler incoming stack alignment is not RSP%16==8 or ESP%16==12
+__attribute__((force_align_arg_pointer)) void _start()
+{
+    asm("call main");
+
+    /* exit system call */
+    asm("mov $0, %rdi;"
+        "mov %eax, %edi;"
+        "mov $0x3c, %rax;"
+        "syscall");
+    // tell the compiler to make sure side effects are done before the asm statement
+    __builtin_unreachable();
+}
