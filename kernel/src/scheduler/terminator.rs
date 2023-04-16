3132f7747f4bd546d8037d5bef1d26cb66ba2883
NoamBroza <noambroza@gmail.com>
2023-04-16 20:00:15 +0300

    a fix for terminate_from_queue function

--- kernel/src/scheduler/terminator.rs
+++ kernel/src/scheduler/terminator.rs
@@ -9,6 +9,7 @@ pub unsafe fn add_to_queue(p: Process) {
 
 pub extern "C" fn terminate_from_queue(_: *mut u64) -> i32 {
     loop {
+        for _ in 0..20 {} // wait a little, else the TERMINATE_PROC_QUEUE mutex will corrupt and the system will crash
         unsafe { TERMINATE_PROC_QUEUE.lock() }.dequeue();
     }
 }
