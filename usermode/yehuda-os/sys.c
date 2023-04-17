19aac128ad869d453663dd6cc9f9ea6f03eea396
Omer <omeralon53@gmail.com>
2023-04-17 20:14:21 +0300

    Fix the `syscall` function

--- usermode/yehuda-os/sys.c
+++ usermode/yehuda-os/sys.c
@@ -23,16 +23,21 @@ size_t
 syscall(size_t syscall_number, size_t arg0, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5)
 {
     size_t result;
+    register long rax asm("rax") = syscall_number;
+    register long rdi asm("rdi") = arg0;
+    register long rsi asm("rsi") = arg1;
+    register long rdx asm("rdx") = arg2;
     register long r10 asm("r10") = arg3;
-    register long r8 asm("r10")  = arg4;
-    register long r9 asm("r10")  = arg5;
+    register long r8 asm("r8")   = arg4;
+    register long r9 asm("r9")   = arg5;
 
-    asm volatile("syscall" ::"%rax"(syscall_number), "%rdi"(arg0), "%rsi"(arg1),
-    "%rdx"(arg2), "r"(r10), "r"(r8), "r"(r9));
+    asm volatile("syscall" ::"r"(rax), "r"(rdi), "r"(rsi), "r"(rdx), "r"(r10),
+    "r"(r8), "r"(r9));
     asm("movq %%rax, %0;" : "=r"(result));
 
     return result;
 }
+
 
 /**
  * Read bytes from a file descriptor.
