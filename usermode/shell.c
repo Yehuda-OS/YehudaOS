43e1b12530ecc056afcc9645fd008f08ce7f692a
Omer <omeralon53@gmail.com>
2023-04-17 20:38:21 +0300

    Change welcome message

--- usermode/shell.c
+++ usermode/shell.c
@@ -263,7 +263,7 @@ bool_t handle_command()
 
 int main()
 {
-    print_str("Welcome to YehudaSH\n");
+    print_str("Welcome to YehudaSH!\n");
     while (TRUE)
     {
         if (!handle_command())
