a30800ba5d0c2f0473c3f59f25b31eb1a52e50f9
Omer <omeralon53@gmail.com>
2023-04-21 12:05:12 +0300

    Fix bugs in the `ls` app

--- usermode/ls.c
+++ usermode/ls.c
@@ -1,57 +1,35 @@
-#include "yehuda-os/sys.h"
 #include "yehuda-os/helpers.h"
+#include "yehuda-os/sys.h"
 
 #define BUF_SIZE 1024
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    // let mut ans: DirList = vec![];
-    // let mut entry: &mut DirListEntry = &mut DirListEntry {
-    //     name: "",
-    //     is_dir: false,
-    //     file_size: 0,
-    // };
+    int fd                  = open(argc > 1 ? "." : argv[1]);
+    struct Stat ls_dir_stat = { .size = 0, .directory = 0 };
+    struct Stat child_stat  = { .size = 0, .directory = 0 };
+    struct DirEntry entry   = { .id = 0, .name = 0 };
 
-    // let dir = get_inode(path_str, None).unwrap();
-    // let mut data: Vec<u8> = vec![0; dir.size()];
-    // unsafe { read(dir.id(), data.as_mut_slice(), 0) };
-    // let dir_content = unsafe {
-    //     Box::from(slice::from_raw_parts(
-    //         data.as_ptr() as *const DirEntry,
-    //         data.len() / core::mem::size_of::<DirEntry>(),
-    //     ))
-    // };
-    // let file = Inode::default();
-
-    // for i in 0..dir_content.len() {
-    //     entry.name = Box::leak(
-    //         String::from_utf8(dir_content[i].name.to_vec())
-    //             .unwrap()
-    //             .into_boxed_str(),
-    //     );
-    //     unsafe {
-    //         blkdev::read(
-    //             get_inode_address(dir_content[i].id),
-    //             core::mem::size_of::<Inode>(),
-    //             &file as *const _ as *mut u8,
-    //         )
-    //     };
-    //     entry.file_size = file.size();
-    //     entry.is_dir = file.is_dir();
-    //     ans.push(entry.clone());
-    // }
-
-    // ans
-    int fd = open(argc == 1 ? "/." : argv[1]);
-    struct Stat stat;
-    fstat(fd, &stat);
-    struct dirent *entry = {0, 0};
-
-    readdir(fd, 0, entry);
-    int offset = 0;
-    for (; offset < stat.size; offset += sizeof(struct DirEntry))
+    if (fstat(fd, &ls_dir_stat) == -1)
     {
-        print_str(((struct DirEntry *)((struct DirEntry *)entry + offset))->name);
+        print_str("ls: directory does not exist\n");
+
+        return 1;
+    }
+
+    for (size_t i = 0; i < ls_dir_stat.size; i++)
+    {
+        if (readdir(fd, 0, &entry) == -1 || fstat((int)entry.id, &child_stat) == -1)
+        {
+            print_str("ls: failed to read directory\n");
+
+            return 1;
+        }
+        print_str(entry.name);
+        if (child_stat.directory)
+        {
+            print_str("/");
+        }
         print_newline();
     }
 
